16efddb8516d78665f681ff44ddfff3a9cb10c17
dhru-conestoga <dghiya@conestogac.on.ca>
2024-02-20 18:25:27 -0500

    Home page UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,49 +4,44 @@ import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap
 import "../styles/Home.css";
 import logo from '../assets/logo.png'; // Import your logo
+import picture from '../assets/Dashboard.webp'; // Import your logo
 
 const Home = () => {
-
-  const { currentUser, LogOut} = useAuth();
-
+  const { currentUser, LogOut } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
-      // Use Firebase auth to sign out
       await LogOut();
       navigate('/');
-      console.log("logged out");
-      // Redirect or handle successful logout
+      console.log("Logged out successfully");
     } catch (error) {
-      // Handle logout error
       console.error('Logout failed:', error.message);
     }
   };
 
   const navigateTo = (path) => {
-    navigate(path); // Navigate to the specified path
+    navigate(path);
   };
 
   return (
     <div className="dashboard">
-      <div className="header">
-        <img src={logo} alt="Company Logo" className="logo" />
-        <h2>Welcome to the Home Page, {currentUser.email}</h2>
-        <button onClick={handleLogout}>Logout</button>
-      </div>
-      <div className="navigation">
-        <button className="navigation-tab" onClick={() => navigateTo('/AddDistributor')}>Add Distributor</button>
-        <button className="navigation-tab" onClick={() => navigateTo('/OrderHistory')}>Order History</button>
-        <button className="navigation-tab" onClick={() => navigateTo('/DistributorList')}>Distributor List</button>
-        <button className="navigation-tab" onClick={() => navigateTo('/CreateNewOrder')}>Create New Order</button>
-        <button className="navigation-tab" onClick={() => navigateTo('/OrderHistory')}>Order History</button>
-
-        {/* Add more navigation tabs as needed */}
+      <div className="navbar">
+        <div className="logo">SimpliSupply Logistics</div>
+        <nav>
+          <ul>
+            <li><button className="nav-button" onClick={() => navigateTo('/AddDistributor')}>Add Distributor</button></li>
+            <li><button className="nav-button" onClick={() => navigateTo('/OrderHistory')}>Order History</button></li>
+            <li><button className="nav-button" onClick={() => navigateTo('/DistributorList')}>Distributor List</button></li>
+            <li><button className="nav-button" onClick={() => navigateTo('/CreateNewOrder')}>Create New Order</button></li>
+          </ul>
+        </nav>
+        <button className="logout-button" onClick={handleLogout}>Logout</button>
       </div>
       <div className="content">
         {/* Add your main content here */}
       </div>
+      <img src={picture} alt="Picture" className="picture" />
     </div>
   );
 };
