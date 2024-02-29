896d46da8b25c2a05dd23cc15558c3f66ceb3946
Purv Pandya <ppurvpandya@gmail.com>
2024-02-29 17:01:11 -0500

    Add inventory data to home page

--- src/firebase/firebaseFirestore.js
+++ src/firebase/firebaseFirestore.js
@@ -227,7 +227,7 @@ export const CreateNewOrderForStore = async (storeID, distributorID, orderItems)
 
             // Update total cost
             totalCost += item.productCost;
-            
+
             const itemRef = doc(collection(orderRef, "orderItems")); // Construct a new DocumentReference for orderItems
             batch.set(itemRef, item);
         });
@@ -284,4 +284,35 @@ export const FetchUserData = async (uuid) => {
     const userData = userDataSnapshot.data();
 
     return userData;
+}
+
+export const FetchStoreInventory = async (storeID) => {
+    try {
+        var storeData = {};
+
+        // Loop through each distributor ID and fetch its document
+        const storeRef = doc(db, 'Retail Stores', storeID);
+        const storeSnapshot = await getDoc(storeRef);
+
+        if (storeSnapshot.exists()) {
+            storeData = storeSnapshot.data();
+            const subcollectionRef = collection(storeRef, "Inventory");
+            const subcollectionSnapshot = await getDocs(subcollectionRef);
+
+            const subcollectionData = subcollectionSnapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            }));
+
+            storeData.inventory = subcollectionData;
+        } else {
+            console.log(`Document with ID ${storeID} does not exist.`);
+        }
+
+        return storeData.inventory;
+    } catch (error) {
+        console.error('Error fetching store information:', error);
+        throw error; // Rethrow the error to handle it at a higher level
+    }
+
 }
