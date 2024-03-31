449d0e7a266aeae84efd7375fd739bd38473844f
Purv Pandya <ppurvpandya@gmail.com>
2024-03-31 13:24:38 -0400

    Add validation to adding store

--- src/pages/AddStore.jsx
+++ src/pages/AddStore.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useAuth } from '../firebase/firebaseAuth';
 import { useNavigate } from 'react-router-dom';
 import { AddNewDistributionStoreForOperator, AddNewStoreForOperator } from '../firebase/firebaseFirestore';
-import { message } from 'antd';
+import { Button, message } from 'antd';
 import MainNavBar from "../components/MainNavBar";
 import BusinessDetails from "../components/BusinessDetails";
 
@@ -29,6 +29,39 @@ const AddStore = () => {
 
   const handleStore = async () => {
     try {
+      // Validate input fields before adding a new store
+      if (!businessName || !businessAddress || !businessNumber || !gstNumber || !businessContact || !businessCity || !businessPostalCode || !businessProvince) {
+        message.error('Please fill in all required fields before adding a new store.');
+        return;
+      }
+
+      if (businessNumber.length !== 9) {
+        message.error('BIN should be 9 digits');
+        return;
+      }
+
+      // Validate GST/HST number format
+      const gstRegex = /^\d{9}[A-Za-z]{2}\d{4}$/; // Assuming GST/HST number format
+      if (!gstRegex.test(gstNumber)) {
+        message.error('Please enter a valid GST/HST number (9 digits BN followed by program code and a 4-digit reference number)');
+        return;
+      }
+
+      // Validate postal code format
+      const postalCodeRegex = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/; // Canadian postal code format
+      if (!postalCodeRegex.test(businessPostalCode)) {
+        message.error('Please enter a valid Canadian Postal Code');
+        return;
+      }
+
+      // Validate contact number format
+      const contactRegex = /^\d{10}$/; // Assuming contact number should be 10 digits
+      if (!contactRegex.test(businessContact)) {
+        message.error('Please enter a valid contact number (10 digits)');
+        return;
+      }
+
+
       const uuid = currentUser.user.uid;
 
       let addStoreFunction;
@@ -93,7 +126,7 @@ const AddStore = () => {
             setBusinessProvince={setBusinessProvince}
           />
           <div className="d-grid gap-2 mt-3">
-            <button variant="primary" onClick={handleStore}>ADD</button>
+            <Button type="primary" style={{marginBottom: 30}} onClick={handleStore}>ADD</Button>
           </div>
         </div>
       </div>
